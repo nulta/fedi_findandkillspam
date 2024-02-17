80a18573c66d7c3e0fa13974cae7b23a15e619d1
nulta <un5450@outlook.com>
2024-02-17 18:05:54 +0900

    Rev 4

--- misskey_find_and_kill_spam.ts
+++ misskey_find_and_kill_spam.ts
@@ -1,4 +1,4 @@
-// Rev 3
+// Rev 4
 // for Misskey
 // Required permissions:
 //   write:notes
@@ -8,20 +8,25 @@
 
 
 const SERVER_URL = "https://misskey.example.com/"
-const API_ACCESS_TOKEN = "InsertApiAccessTokenHere"
+const API_ACCESS_TOKEN = "InsertApiKeyHere"
 
 // Other settings
-const NEW_ACCOUNT_THRESHOLD = 1000 * 60 * 20
-const MENTIONS_THRESHOLD = 5
+const NEW_ACCOUNT_THRESHOLD = 1000 * 60 * 120
+const MENTIONS_THRESHOLD_1 = 2
+const MENTIONS_THRESHOLD_2 = 5
+const IMAGE_OCCURRENCE_THRESHOLD = 4
 
 const FETCH_DELAY = 1000
 const REPORT_REPEATS = 10
+const IMAGE_OCCURRENCE_DECAY_REPEATS = 120
+const VERSION_CHECK_REPEATS = 300
 const TESTING_MODE = false
+const VER = 4.0
 
 
 async function requestApi(endpoint: string, body: Record<string, any>) {
     body = {
-        i: API_ACCESS_TOKEN,
+        i: API_ACCESS_TOKEN.replaceAll(/[' ]/g, ""),
         ...body,
     }
     
@@ -55,18 +60,74 @@ function parseAidx(id: string): Date {
 	return new Date(time);
 }
 
+// TODO: rewrite
+const imageOccuranceMap = new Map<string, number>()
+let imageOccuranceNoteId = ""
+let imageOccuranceSameNoteKeys = new Set()
+function imageOccurrenceFilter(blurhash: string | null, noteId: string): boolean {
+    if (blurhash == null) {return false}
+
+    const splits: string[] = blurhash.match(/.{1,6}/g) ?? []
+    let detections = 0
+
+    if (imageOccuranceNoteId != noteId) {
+        imageOccuranceNoteId = noteId
+        imageOccuranceSameNoteKeys = new Set()
+    }
+
+    splits.forEach((v, i) => {
+        const key = `${i} ${v}`
+        if (imageOccuranceSameNoteKeys.has(key)) { return }
+        imageOccuranceSameNoteKeys.add(key)
+
+        const occurrences = imageOccuranceMap.get(key) ?? 0
+        imageOccuranceMap.set(key, occurrences + 1)
+
+        if (occurrences+1 >= IMAGE_OCCURRENCE_THRESHOLD) {
+            detections += 1
+        }
+    })
+
+    // 6 of 9
+    if (detections >= 6) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function imageOccurrenceDecay() {
+    new Map(imageOccuranceMap).forEach((v, k) => {
+        v -= 1
+        if (v <= 0) {
+            imageOccuranceMap.delete(k)
+        } else {
+            imageOccuranceMap.set(k, v)
+        }
+    })
+}
+
 function shouldKillNote(note: Record<string, any>) {
     const {id, userId, visibility, user, text } = note
     const mentions: Array<string> = note["mentions"] ?? []
+    const files: Array<any> = note["files"] ?? []
 
-    const conditions = [
-        () => visibility == "public",
-        () => mentions.length >= MENTIONS_THRESHOLD,
+    const hardConditions = [
+        () => mentions.length >= MENTIONS_THRESHOLD_1,
         () => parseAidx(userId).getTime() >= (Date.now() - NEW_ACCOUNT_THRESHOLD),
-        () => user["avatarBlurhash"] == null,
-    ]
+    ].map(f => f())
 
-    return conditions.every(f => f())
+    if (!hardConditions.every(b => b)) { return false }
+    
+    const softConditions = [
+        () => visibility == "public",
+        () => mentions.length >= MENTIONS_THRESHOLD_2,
+        () => user["avatarBlurhash"] == null,
+        () => files.map(f => imageOccurrenceFilter(f["blurhash"] ?? null, id)).some(b => b),
+        () => (user.name == user.username) || (user.name == null),
+    ].map(f => f())
+
+    return (softConditions.filter(b => b).length >= 3)
 }
 
 function killNote(note: Record<string, any>) {
@@ -78,25 +139,46 @@ function killNote(note: Record<string, any>) {
     )
 
     console.log(
-        `    Content: ${text.slice(0, 100).replaceAll("\n", "\\n")}`
+        `    Content: ${text.slice(0, 150).replaceAll("\n", "\\n")}`
     )
 
     requestApi("notes/delete", {noteId: id})
     requestApi("admin/suspend-user", {userId: userId})
 }
 
+async function checkVersion() {
+    try {
+        const uri = "https://raw.githubusercontent.com/nulta/fedi_findandkillspam/main/VERSION"
+        const text = await fetch(uri).then(r => r.text())
+        const newVer = parseFloat(text)
+        if (VER < newVer) {
+            console.log(`[${new Date().toISOString()}] `, "")
+            console.log(`[${new Date().toISOString()}] `, "New update available!!!")
+            console.log(`[${new Date().toISOString()}] `, "Current version:", VER)
+            console.log(`[${new Date().toISOString()}] `, "New version:", newVer)
+            console.log(`[${new Date().toISOString()}] `, "")
+        }
+    } catch(e) {
+        console.error(`[${new Date().toISOString()}] `, "Failed to fetch version information.")
+    }
+}
+
 
 let lastId = undefined
-let reports = REPORT_REPEATS
+let reports = 0
 let processedNotes = 0
-
 async function fetchNotes() {
     try {
-        const notes: Array<Record<string, any>> = await requestApi("notes/global-timeline", {
+        const notes: Array<Record<string, any>> | undefined = await requestApi("notes/global-timeline", {
             withRenotes: false,
             sinceId: lastId,
             limit: 100,
         })
+
+        if (notes === undefined) {
+            console.error(`[${new Date().toISOString()}] `, "Fetch failed?! Got undefined response.")
+            return
+        }
     
         notes.forEach(note => {
             if (shouldKillNote(note)) {
@@ -106,12 +188,19 @@ async function fetchNotes() {
         })
 
         processedNotes += notes.length
-        reports -= 1
+        reports += 1
 
-        if (reports <= 0) {
+        if (reports % REPORT_REPEATS == 0) {
             console.log(`[${new Date().toISOString()}] `, `processed ${processedNotes} notes.`)
             processedNotes = 0
-            reports = REPORT_REPEATS
+        }
+
+        if (reports % IMAGE_OCCURRENCE_DECAY_REPEATS == 0) {
+            imageOccurrenceDecay()
+        }
+
+        if (reports % VERSION_CHECK_REPEATS == 0) {
+            checkVersion()
         }
     } catch (e) {
         console.error(`[${new Date().toISOString()}] `, e)
@@ -119,5 +208,6 @@ async function fetchNotes() {
 }
 
 setInterval(fetchNotes, FETCH_DELAY)
-console.log(`[${new Date().toISOString()}] `, "find_and_kill_spam: rev 3")
+console.log(`[${new Date().toISOString()}] `, "find_and_kill_spam: rev", VER)
 console.log(`[${new Date().toISOString()}] `, "find_and_kill_spam: Started!")
+checkVersion()
